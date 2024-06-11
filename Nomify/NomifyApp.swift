1616a28a4a77e748321ce904c1038dac93b70217
Andy Vu <andyvu2026@u.northwestern.edu>
2024-06-11 10:52:30 -0500

    Initial Commit

---
+++ Nomify/NomifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NomifyApp.swift
+//  Nomify
+//
+//  Created by Andy Vu on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct NomifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
